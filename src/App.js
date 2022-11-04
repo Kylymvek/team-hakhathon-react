0d7b57c584680845190569d2bfc905d0c9fb87de
user <user@users-MacBook-Air-2.local>
2022-11-04 16:51:11 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
