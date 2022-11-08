01b06e260a8f07e3466d4d1fe511d0ae42541da6
user <user@users-MacBook-Air-2.local>
2022-11-08 14:04:42 +0600

    swiper

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import HandleSwiper from "./components/Swiper/Swiper";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <HandleSwiper />
       <Footer />
     </>
+  );
 };
 
 export default App;
---
+++ src/components/Swiper/Swiper.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Navigation, Pagination } from "swiper";
+import asus from "./Image/asus.jpeg";
+import assasin from "./Image/assasin.jpeg";
+import legion from "./Image/legion.jpeg";
+import tuf from "./Image/tuf.gif";
+import ryzen from "./Image/ryzen.jpeg";
+import msi from "./Image/msi.jpeg";
+import nvidia from "./Image/nvidia.jpeg";
+import groot from "./Image/groot.jpeg";
+import razer from "./Image/razer.jpeg";
+
+const HandleSwiper = () => {
+  return (
+    <>
+      <Swiper
+        style={{ width: "80%" }}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 1000000000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper">
+        <SwiperSlide>
+          <img src={tuf} alt="logo" style={{ width: "100%", height: "100%" }} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={razer}
+            alt="logo"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={groot}
+            alt="logo"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={nvidia}
+            alt="logo"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={msi} alt="logo" style={{ width: "100%", height: "100%" }} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={ryzen}
+            alt="logo"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={asus}
+            alt="logo"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={assasin}
+            alt="logo"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={legion}
+            alt="logo"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default HandleSwiper;
